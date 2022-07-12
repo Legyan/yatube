9a55e3f84c1abf613e178f0f71c653b747a36a80
Levon Gegamyan <levongeg@yandex.ru>
2022-07-12 13:18:34 +0300

    version 3.1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .forms import PostForm, CommentForm
-from .models import Post, Group, Comment, User, Follow
+from .models import Post, Group, User, Follow
 from .paginator import post_paginator
 
 
