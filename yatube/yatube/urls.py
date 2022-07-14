0c441c26e19e94900026ccb07fdf26c0476e4f73
Levon Gegamyan <levongeg@yandex.ru>
2022-07-14 09:54:41 +0300

    add django_debug_toolbar

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
-from django.conf.urls.static import static
 
 handler404 = 'core.views.page_not_found'
 handler403 = 'core.views.csrf_failure'
@@ -31,6 +30,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+    import debug_toolbar
+
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
