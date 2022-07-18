d7408bc0b19c3cb4dad7c6ed81f0e670456a193e
Levon Gegamyan <levongeg@gmail.com>
2022-07-18 19:22:16 +0300

    fixed "about"

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
+from django.conf.urls.static import static
 
 handler404 = 'core.views.page_not_found'
 handler403 = 'core.views.csrf_failure'
@@ -31,5 +32,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
-
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
